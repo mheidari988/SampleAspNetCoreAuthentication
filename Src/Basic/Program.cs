68fb09b10d0033b75a2690cf8f42390e45027157
Mohammadreza Heidari <m.heidari988@gmail.com>
2022-04-18 16:08:43 +0300

    Add authorization policy

--- Src/Basic/Program.cs
+++ Src/Basic/Program.cs
@@ -1,37 +1,58 @@
 using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc.Authorization;
+using System.Security.Claims;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddLogging(cnfg =>
-{
-    cnfg.AddConsole()
-        .AddDebug();
-});
-builder.Services.AddControllersWithViews(cnfg =>
-    {
-        cnfg.Filters.Add(new AuthorizeFilter());
-    })
-    .AddRazorRuntimeCompilation();
-
-builder.Services.AddAuthentication(CookieAuthenticationDefaults.CookiePrefix)
-    .AddCookie(CookieAuthenticationDefaults.CookiePrefix, cnfg =>
-    {
-        cnfg.LoginPath = "/Home/Authenticate";
-        cnfg.Cookie.Name = "MyCuteCookie";
-    });
-
+ConfigLogging(builder);
+ConfigControllersWithViews(builder);
+ConfigAuthentication(builder);
+ConfigAuthorization(builder);
 
 var app = builder.Build();
 
 app.UseRouting();
-
 app.UseAuthentication();
 app.UseAuthorization();
-
 app.UseEndpoints(endpoints =>
 {
     endpoints.MapDefaultControllerRoute();
 });
-
 app.Run();
+
+static void ConfigLogging(WebApplicationBuilder builder)
+{
+    builder.Services.AddLogging(cnfg =>
+    {
+        cnfg.AddConsole()
+            .AddDebug();
+    });
+}
+static void ConfigControllersWithViews(WebApplicationBuilder builder)
+{
+    builder.Services.AddControllersWithViews(cnfg =>
+    {
+        cnfg.Filters.Add(new AuthorizeFilter());
+    })
+        .AddRazorRuntimeCompilation();
+}
+static void ConfigAuthentication(WebApplicationBuilder builder)
+{
+    builder.Services.AddAuthentication(CookieAuthenticationDefaults.CookiePrefix)
+        .AddCookie(CookieAuthenticationDefaults.CookiePrefix, cnfg =>
+        {
+            cnfg.LoginPath = "/Home/Authenticate";
+            cnfg.Cookie.Name = "MyCuteCookie";
+        });
+}
+static void ConfigAuthorization(WebApplicationBuilder builder)
+{
+    builder.Services.AddAuthorization(cnfg =>
+    {
+        cnfg.DefaultPolicy = new AuthorizationPolicyBuilder()
+            .RequireAuthenticatedUser()
+            .RequireClaim(ClaimTypes.DateOfBirth)
+            .Build();
+    });
+}
