cc1240b073df0a29e4ea51cc637de8fa86c829e5
Mohammadreza Heidari <m.heidari988@gmail.com>
2022-04-18 23:50:53 +0300

    Add custom authorization policy

---
+++ Src/Basic/AuthorizationRequirements/CustomPolicyExtensions.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Authorization;
+
+namespace Basic.AuthorizationRequirements
+{
+    public static class CustomPolicyExtensions
+    {
+        public static AuthorizationPolicyBuilder AddCustomPolicy(this AuthorizationPolicyBuilder builder, string customClaim)
+        {
+            builder.AddRequirements(new CustomPolicy(customClaim));
+            return builder;
+        }
+    }
+}
