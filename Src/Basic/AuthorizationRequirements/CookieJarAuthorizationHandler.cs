bfed1b3236ae0eb95ba2818a3b6a992af663de39
Mohammadreza Heidari <m.heidari988@gmail.com>
2022-04-19 16:17:27 +0300

    Using OperationAuthorizationRequirement

---
+++ Src/Basic/AuthorizationRequirements/CookieJarAuthorizationHandler.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Authorization.Infrastructure;
+
+namespace Basic.AuthorizationRequirements
+{
+    public class CookieJarAuthorizationHandler :
+        AuthorizationHandler<OperationAuthorizationRequirement>
+    {
+        protected override Task HandleRequirementAsync(
+            AuthorizationHandlerContext context,
+            OperationAuthorizationRequirement requirement)
+        {
+            if (requirement.Name == CookieJarOperations.Look)
+            {
+                var checkAuth = context.User.Identity?.IsAuthenticated;
+                if (checkAuth.HasValue && checkAuth.Value)
+                {
+                    context.Succeed(requirement);
+                }
+            }
+            else if (requirement.Name == CookieJarOperations.ComeNear)
+            {
+                if (context.User.HasClaim("Friend", "Good Friend"))
+                {
+                    context.Succeed(requirement);
+                }
+            }
+            return Task.CompletedTask;
+        }
+    }
+
+}
